856d106841971e342a5b493b2db0845a310841e2
razzg946-cmd <razzg946@gmail.com>
2026-06-19 10:58:17 +0530

    Update speech_to_speecha.py

--- speech_to_speecha.py
+++ speech_to_speecha.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="centered"
 )
 
-st.image("icon.png", width=150)
+st.image("icon.png", width=110)
 
 st.title("🌍 Rvoice - GulfTalk Translator")
 st.write("Founder & CEO - Raj Gupta")
